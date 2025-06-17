7c6a8d46902644a9f52ad995dd9f8639c0bbc353
borjan.gjorgiev <borjangjorgiev2@gmail.com>
2025-06-17 16:12:28 +0200

    From controller to restcontroller

--- src/main/java/com/example/intecproject/controller/GroupController.java
+++ src/main/java/com/example/intecproject/controller/GroupController.java
@@ -2,57 +2,43 @@ package com.example.intecproject.controller;
 
 
 import com.example.intecproject.model.Group;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import com.example.intecproject.service.GroupService;
 import com.example.intecproject.service.UserService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/api/gpoups")
 public class GroupController {
-
     private final GroupService groupService;
     private final UserService userService;
-
 
     public GroupController(GroupService groupService, UserService userService) {
         this.groupService = groupService;
         this.userService = userService;
     }
+
     @GetMapping("/groups")
-    public String listAllGroups(Model model)
-    {
-        model.addAttribute("groups",groupService.findAll());
-        return "groups";
+    public List<Group> listAllGroups() {
+        return groupService.findAll();
     }
 
-
     @PostMapping("/delete/{id}")
-    public String deleteGroup(@PathVariable Long id,Model model)
+    public ResponseEntity<Void> deleteGroup(@PathVariable Long id)
     {
         groupService.deleteGroup(id);
-        return "redirect:/groups";
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}")
-    public String getDetailsForGroup(@PathVariable Long id,Model model)
+    public ResponseEntity<Group> getDetailsForGroup(@PathVariable Long id)
     {
-       Group g=groupService.findById(id);
-
-       model.addAttribute("group",g);
-        return "groups-detail";
+        return ResponseEntity.ok(groupService.findById(id));
     }
-
-
-
-
-
-
-
-
-
 
 
 }
--- src/main/java/com/example/intecproject/controller/UserController.java
+++ src/main/java/com/example/intecproject/controller/UserController.java
@@ -16,57 +16,41 @@ import java.util.List;
 @RestController
 
 public class UserController {
-
     private final UserService userService;
     private final GroupService groupService;
-
-
     public UserController(UserService userService, GroupService groupService) {
         this.userService = userService;
         this.groupService = groupService;
     }
-
-
     @GetMapping("/users")
     public List<User> listAllUsers()
     {
       return  userService.findAll();
     }
-
-
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user)
     {
         return ResponseEntity.ok(userService.save(user));
     }
-
     @DeleteMapping("/users/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id)
     {
         userService.delete(id);
         return ResponseEntity.noContent().build();
     }
-
     @GetMapping("/users/{id}")
     public ResponseEntity<User> getDetailsForUser(@PathVariable Long id)
     {
         return ResponseEntity.ok(userService.findById(id));
     }
-
     @PostMapping("/{userId}/groups/{groupId}/place")
     public ResponseEntity<?> placeUserInGroup(@PathVariable Long userId, @PathVariable Long groupId) {
         userService.PlaceUserInGroup(userId, groupId);
         return ResponseEntity.ok("User added to group.");
     }
-
     @DeleteMapping("/{userId}/groups/{groupId}/delete")
     public ResponseEntity<?> removeUserFromGroup(@PathVariable Long userId, @PathVariable Long groupId) {
         userService.RemoveUserFromGroup(userId, groupId);
         return ResponseEntity.ok("User removed from group.");
     }
-
-
-
-
-
 }
